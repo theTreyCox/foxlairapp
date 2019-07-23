a94185d3aba7fe1c521f5c41c79226d0cb3445e4
Trey Cox <treycox@gmail.com>
2019-07-22 20:39:35 -0400

    leaf file added

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Leaf
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -8,8 +9,8 @@ public func routes(_ router: Router) throws {
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello") { req -> Future<View> in
+        return try req.view().render("home")
     }
 
     // Example of configuring a controller
